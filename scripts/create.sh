99db5e5e2e6dcc143ba916b2a61f23234d530b4d
orfeon <orfeon@mercari.com>
2018-10-02 15:22:07 +0900

    remove inuse files.

--- scripts/create.sh
+++
@@ -1,9 +0,0 @@
-mvn archetype:generate \
-      -DarchetypeGroupId=org.apache.beam \
-      -DarchetypeArtifactId=beam-sdks-java-maven-archetypes-examples \
-      -DarchetypeVersion=2.6.0 \
-      -DgroupId=net.orfeon.cloud \
-      -DartifactId=templates \
-      -Dversion="0.1" \
-      -Dpackage=net.orfeon.cloud.dataflow \
-      -DinteractiveMode=false
